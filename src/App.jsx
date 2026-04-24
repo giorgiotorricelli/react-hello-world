f4df6283fb9e04347f46a011a76b3f58a9cf0154
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-24 16:13:09 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css'
+const pippo = "ciao";
+
+const coloriCubo = ["white", "yellow", "red", "orange", "blue", "green"];
 
 function App() {
-  return <h1>Title</h1>;
+
+  return <>
+    <h1>Rubik</h1>
+    <p>{pippo}</p>
+  </>
+  
+  ;
 }
 
 export default App
